65cb8cbc38f645134986dc2f73147d3fb71164bc
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2026-03-21 18:52:46 -0300

    Vitest configuration

---
+++ domain/backend/auth/vitest.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config'
+import rootConfig from '../../../vitest.config.js'
+
+export default defineConfig({
+    ...rootConfig,
+    test: {
+        ...rootConfig.test,
+        environment: 'node'
+    }
+})
