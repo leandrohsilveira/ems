ce4d8367455fa28f6b5f7e7fb8bdac75c5b1d8a1
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2026-03-21 12:49:54 -0300

    Setup prettier and eslint on API app.

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,42 +1,42 @@
 import Fastify from 'fastify';
 
 const fastify = Fastify({
-  logger: {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        translateTime: 'HH:MM:ss Z',
-        ignore: 'pid,hostname'
-      }
-    }
-  }
+	logger: {
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				translateTime: 'HH:MM:ss Z',
+				ignore: 'pid,hostname'
+			}
+		}
+	}
 });
 
 /** @type {import('fastify').RouteShorthandOptions} */
 const helloWorldOptions = {
-  schema: {
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          message: { type: 'string' }
-        }
-      }
-    }
-  }
+	schema: {
+		response: {
+			200: {
+				type: 'object',
+				properties: {
+					message: { type: 'string' }
+				}
+			}
+		}
+	}
 };
 
 fastify.get('/', helloWorldOptions, async () => {
-  return { message: 'Hello World' };
+	return { message: 'Hello World' };
 });
 
 const start = async () => {
-  try {
-    await fastify.listen({ port: 3000 });
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+	try {
+		await fastify.listen({ port: 3000 });
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
 };
 
 start();
